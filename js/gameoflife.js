865ec5cf4def73a938f6213f662f7c8488bcd9ea
wafflebag <40217899+wafflebag@users.noreply.github.com>
2020-02-25 15:18:53 -0800

    Update gameoflife.js

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -16,19 +16,13 @@ const printCell = (cell, state) => {
 };
 
 const corners = (state = []) => {
-  isFinite(state.length === 0) {
+  isFinite(state.length === 0) 
     return {
       topRight: [0,0],
       bottomLeft: [0,0]
     }
-  }
-  const xs = state.map(([x, _]) => x);
-  const ys = state.map(([_, y]) => y);
-  return {
-    topRight: [Math.max(...xs), Math.max(...ys)],
-    bottomLeft: [Math.min(...xs), Math.min(...ys)]
   };
-};
+
 
 const printCells = (state) => {
   const {bottomLeft, topRight} = corners(state);
